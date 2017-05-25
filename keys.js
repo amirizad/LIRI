c4138a6e240f0250835d26889231e186bad8f8fe
amirizad <info@amirizad.com>
2017-05-25 14:04:47 -0700

    My workspace setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Psa9lTxfmfPoMulGPAHQDFME5',
+  consumer_secret: '6Z9SkwKpb0DDfVNQpPIf2g2iFxblZvplUrJfvSceVtwY6OEmle',
+  access_token_key: '867845726876844036-tdLUxOo7cy37m1VUfuHdBX5SdA4CMeu',
+  access_token_secret: '00YAZkCwgkfTKTusiy2HSlrd28GQiD4lwqW0SVeQqH0bJ',
+}
